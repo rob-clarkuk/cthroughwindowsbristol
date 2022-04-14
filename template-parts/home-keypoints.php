cba29dd6d773de5ad47a878e7c250c269b554568
Rob Clark <rob@clarkuk.com>
2022-04-14 17:28:37 +0100

    Key Points Section

--- template-parts/home-keypoints.php
+++ template-parts/home-keypoints.php
@@ -1,3 +1,10 @@
-<section class="keypoints pb1 pt1">
-
-</section>
+<?php
+	if( have_rows('key_points') ):?>
+	<section class="keypoints pb1 pt1">
+		<div class="container">
+		<?php while( have_rows('key_points') ) : the_row();?>
+			<div><?php the_sub_field('title');?></div>
+		<?php endwhile;?>
+		</div>
+	</section>
+<?php endif;?>
