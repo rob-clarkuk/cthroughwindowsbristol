705219a650644db8289d3a29cea13febd1385d6a
Rob Clark <rob@clarkuk.com>
2022-04-14 15:29:53 +0100

    Hero HTML

--- template-parts/hero-home.php
+++ template-parts/hero-home.php
@@ -1,1 +1,28 @@
-hero-home.php
+<?php 
+	$heroImage = get_field('hero_image');
+if( !empty( $image ) ): ?>
+    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+<?php endif; ?>
+
+<section class="hero" style="background-image: url(<?php echo esc_url($heroImage['url']); ?>);">
+	<?php if(get_field('hero_title')){ ;?>
+		<div class="hero__title">
+			<h1><?php the_field('hero_title');?></h1>
+		</div>
+	<?php } ;?>
+	<?php if(get_field('hero_sub_title')){ ;?>
+		<div class="hero__subtitle">
+			<h2><?php the_field('hero_sub_title');?></h2>
+		</div>
+	<?php } ;?>
+	<?php
+		if( have_rows('hero_buttons') ):?>
+			<div class="hero__cta">
+			<?php while( have_rows('hero_buttons') ) : the_row();?>
+				<a href="<?php the_sub_field('button_link');?>">
+					<?php the_sub_field('button_colour');?>
+				</a>
+			<?php endwhile;?>
+			</div>
+		<?php endif;?>
+</section>
