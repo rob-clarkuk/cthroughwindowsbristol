cf6cd2f73d51142a3af96d8fb82c9b8a2468c5d4
Rob Clark <rob@clarkuk.com>
2022-04-14 17:42:15 +0100

    Key Points Section

--- template-parts/home-keypoints.php
+++ template-parts/home-keypoints.php
@@ -3,7 +3,7 @@
 	<section class="keypoints pb1 pt1">
 		<div class="container keypoints__container">
 		<?php while( have_rows('key_points') ) : the_row();?>
-			<div class="keypoints__item"><?php the_sub_field('title');?></div>
+			<div class="keypoints__item text-center pb2 pl2 pr2 pt2"><?php the_sub_field('title');?></div>
 		<?php endwhile;?>
 		</div>
 	</section>
