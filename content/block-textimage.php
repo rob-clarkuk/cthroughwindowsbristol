196005e56aee92d5fc890c428836f38ce852ee57
Rob Clark <rob@clarkuk.com>
2022-04-18 17:22:19 +0100

    Text Image Bloxk setup

---
+++ content/block-textimage.php
@@ -0,0 +1,7 @@
+<section class="textimage grid3 grid pb2 pl2 pr2 pt2">
+	<div class="textimage__content section__grey">
+		<?php if(get_sub_field('title')){ ;?>
+			<h3><?php the_sub_field('title');?></h3>
+		<?php } ;?>
+	</div>
+</section>
--- template-parts/page-content.php
+++ template-parts/page-content.php
@@ -4,8 +4,14 @@
 	<?php while ( have_rows('content_section') ) : the_row();?>
 
 		<?php if( get_row_layout() == 'image_text_bullet_points_block' ):?>
-			
+
 			<?php get_template_part( 'content/block', 'itb' );?>
+
+		<?php endif ;?>
+
+		<?php if( get_row_layout() == 'text_image_block' ):?>
+			
+			<?php get_template_part( 'content/block', 'textimage' );?>
 
 		<?php endif ;?>
 
