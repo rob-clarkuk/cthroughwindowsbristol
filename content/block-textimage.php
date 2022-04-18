31851f4f5d700222779841549a51e4ab7eca6b2b
Rob Clark <rob@clarkuk.com>
2022-04-18 17:27:16 +0100

    Text Image Block setup

--- content/block-textimage.php
+++ content/block-textimage.php
@@ -1,5 +1,5 @@
-<section class="textimage grid3 grid pb2 pl2 pr2 pt2">
-	<div class="textimage__content section__lightgrey">
+<section class="textimage grid3 grid">
+	<div class="textimage__content section__lightgrey pb2 pl2 pr2 pt2">
 		<?php if(get_sub_field('title')){ ;?>
 			<h3 class="heading__lg heading__body-font font400 heading__grey"><?php the_sub_field('title');?></h3>
 		<?php } ;?>
@@ -7,4 +7,10 @@
 			<div><?php the_sub_field('content');?></div>
 		<?php } ;?>
 	</div>
+	<?php if(get_sub_field('image')){ ;?>
+		<?php $image = get_sub_field('image');?>
+		<div class="textimage__image">
+			<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_url($image['alt']); ?>">
+		</div>
+	<?php } ;?>
 </section>
