ff2babaa868bf247898196f25d9de076af78ed86
Rob Clark <rob@clarkuk.com>
2022-04-18 17:05:05 +0100

    Image Text Bullets Block setup

--- content/block-itb.php
+++ content/block-itb.php
@@ -1,7 +1,7 @@
 <section class="itb section__lightgrey pb2 pl2 pr2 pt2">
 	<div class="grid3 grid">
 		<?php if(get_sub_field('title')){ ;?>
-			<div class="">
+			<div class="itb__title">
 				<h2 class="heading__lg heading__body-font text__center font400 heading__grey"><?php the_sub_field('title');?></h2>
 			</div>
 		<?php } ;?>
