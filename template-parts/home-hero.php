dbc1c2cc987f1c09eb628f372ebe6197e6c5ed1b
Rob Clark <rob@clarkuk.com>
2022-04-18 16:15:42 +0100

    Standard Page Hero

--- template-parts/home-hero.php
+++ template-parts/home-hero.php
@@ -1,7 +1,7 @@
 <?php 
 	$heroImage = get_field('hero_image');?>
 
-<section class="hero text__center" style="background-image: url(<?php echo esc_url($heroImage['url']); ?>);">
+<section class="hero text__center h50" style="background-image: url(<?php echo esc_url($heroImage['url']); ?>);">
 	<?php if(get_field('hero_title')){ ;?>
 		<div class="hero__title pb1">
 			<h1 class="heading__xl heading__shadow heading__light font100 mb0 mt0"><?php the_field('hero_title');?></h1>
--- template-parts/page-hero.php
+++ template-parts/page-hero.php
@@ -1,9 +1,9 @@
 <?php 
 	$heroImage = get_field('image');?>
 
-<section class="hero text__center" style="background-image: url(<?php echo esc_url($heroImage['url']); ?>);">
-	<?php if(get_field('hero_title')){ ;?>
-		<div class="title pb1">
+<section class="hero text__center pb2 pt2" style="background-image: url(<?php echo esc_url($heroImage['url']); ?>);">
+	<?php if(get_field('title')){ ;?>
+		<div class="hero__title pb1">
 			<h1 class="heading__xl heading__shadow heading__light font100 mb0 mt0"><?php the_field('title');?></h1>
 		</div>
 	<?php } ;?>
