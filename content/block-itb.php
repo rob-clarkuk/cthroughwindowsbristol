3b35674c367058304520476a48df0bae56cd241a
Rob Clark <rob@clarkuk.com>
2022-04-18 17:01:06 +0100

    Image Text Bullets Block setup

--- content/block-itb.php
+++ content/block-itb.php
@@ -2,7 +2,7 @@
 	<div class="grid3 grid">
 		<?php if(get_field('title')){ ;?>
 			<div class="">
-				<h2 class="heading__lg heading__body-font text-center font400 heading__grey"><?php the_field('title');?></h2>
+				<h2 class="heading__lg heading__body-font text__center font400 heading__grey"><?php the_field('title');?></h2>
 			</div>
 		<?php } ;?>
 	</div>
