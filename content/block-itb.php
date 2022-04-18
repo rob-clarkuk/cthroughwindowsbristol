24fd99fc0002cc90256985130850f80f977a317e
Rob Clark <rob@clarkuk.com>
2022-04-18 16:58:06 +0100

    Image Text Bullets Block setup

--- content/block-itb.php
+++ content/block-itb.php
@@ -1,3 +1,9 @@
 <section class="itb section__lightgrey pb2 pl2 pr2 pt2">
-	
+	<div class="grid3 grid">
+		<?php if(get_field('title')){ ;?>
+			<div class="">
+				<h2 class="heading__lg heading__body-font"><?php the_field('title');?></h2>
+			</div>
+		<?php } ;?>
+	</div>
 </section>
