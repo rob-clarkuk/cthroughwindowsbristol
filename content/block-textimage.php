e92ec557f20c2ebcdb489e92aff826fe902f79f1
Rob Clark <rob@clarkuk.com>
2022-04-18 17:25:54 +0100

    Text Image Bloxk setup

--- content/block-textimage.php
+++ content/block-textimage.php
@@ -1,7 +1,10 @@
 <section class="textimage grid3 grid pb2 pl2 pr2 pt2">
 	<div class="textimage__content section__lightgrey">
 		<?php if(get_sub_field('title')){ ;?>
-			<h3 class="heading__lg heading__body-font text__center font400 heading__grey"><?php the_sub_field('title');?></h3>
+			<h3 class="heading__lg heading__body-font font400 heading__grey"><?php the_sub_field('title');?></h3>
+		<?php } ;?>
+		<?php if(get_sub_field('content')){ ;?>
+			<div><?php the_sub_field('content');?></div>
 		<?php } ;?>
 	</div>
 </section>
