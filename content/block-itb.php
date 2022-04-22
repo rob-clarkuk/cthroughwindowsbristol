dba264288126d7e122a5d24251d3b0ce94f3b74c
Rob Clark <rob@clarkuk.com>
2022-04-22 10:06:27 +0100

    block-itb update

--- content/block-itb.php
+++ content/block-itb.php
@@ -13,7 +13,7 @@
 		<?php if(get_sub_field('bullet_points')){ ;?>
 			<div class="itb__bullets">
 				<?php if( have_rows('bullet_points') ): ;?>
-				<ul class="ml0 mb0 pl0 text__left">
+				<ul class="ml0 mb0 pl0">
 					<?php while( have_rows('bullet_points') ) : the_row();?>
 						<li><?php get_template_part( 'icons/icon', 'tick' );?> <?php the_sub_field('bullet_point');?></li>
 					<?php endwhile;?>
