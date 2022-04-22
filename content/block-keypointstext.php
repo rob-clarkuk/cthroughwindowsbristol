07638ce89619565ff056fa24d18e9672c631a13e
Rob Clark <rob@clarkuk.com>
2022-04-22 10:58:47 +0100

    Key Point Text block build

--- content/block-keypointstext.php
+++ content/block-keypointstext.php
@@ -5,5 +5,20 @@
 				<h2 class="heading__lg heading__body-font text__center font400 heading__grey"><?php the_sub_field('title');?></h2>
 			</div>
 		<?php } ;?>
+		<?php if(get_sub_field('key_point')){ ;?>
+			<div class="kpt__content">
+				<?php if( have_rows('key_point') ): ;?>
+					<?php while( have_rows('key_point') ) : the_row();?>
+						<div class="kpt__content--item">
+							<?php if(get_sub_field('title')){ ;?>
+								<div class="kpt__content__title">
+									<h2 class="heading__lg heading__body-font text__center font400 heading__grey"><?php the_sub_field('title');?></h2>
+								</div>
+							<?php } ;?>
+						</div>
+					<?php endwhile;?>
+				<?php endif;?>
+			</div>
+		<?php } ;?>
 	</div>
 </section>
