c5091e6adf874f088f9344b94d2126d9df5d90a7
Rob Clark <rob@clarkuk.com>
2022-04-22 10:51:21 +0100

    Key Point Text block build

--- content/block-keypointstext.php
+++ content/block-keypointstext.php
@@ -1,1 +1,9 @@
-block-keypointstext.php
+<section class="kpt section__lightgrey pb2 pl3 pr3 pt2 mb2">
+	<div class="grid1 grid">
+		<?php if(get_sub_field('title')){ ;?>
+			<div class="kpt__title">
+				<h2 class="heading__lg heading__body-font text__center font400 heading__grey"><?php the_sub_field('title');?></h2>
+			</div>
+		<?php } ;?>
+	</div>
+</section>
