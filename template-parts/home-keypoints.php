2b85346a3bac90c9849bd6ca09657561a025bfb6
Rob Clark <rob@clarkuk.com>
2022-04-25 17:23:44 +0100

    Home Page updates

--- template-parts/home-keypoints.php
+++ template-parts/home-keypoints.php
@@ -3,7 +3,7 @@
 	<section class="keypoints pb1 pt1">
 		<div class="container keypoints__container">
 		<?php while( have_rows('key_points') ) : the_row();?>
-			<div class="keypoints__item text__center pb2 pl2 pr2 pt2" style="background-color:<?php the_sub_field('background_colour');?>">
+			<div class="keypoints__item text__center pb3 pl3 pr3 pt3" style="background-color:<?php the_sub_field('background_colour');?>">
 				<div class="keypoints__item--icon"><?php get_template_part( 'icons/icon', 'tick' );?></div>
 				<div class="keypoints__item--title font700 heading__sm"><?php the_sub_field('title');?></div>
 			</div>
