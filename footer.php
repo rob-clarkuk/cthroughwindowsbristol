4a81fed09240a4dc2c49129f64f379a6d2122624
Rob Clark <rob@clarkuk.com>
2022-04-25 11:28:15 +0100

    Footer

--- footer.php
+++ footer.php
@@ -53,23 +53,12 @@
 				</div>
 	    	<?php endwhile; ?>
 		<?php endif; ?>
-
-
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cthroughwindowsbristol' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cthroughwindowsbristol' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cthroughwindowsbristol' ), 'cthroughwindowsbristol', '<a href="https://robclarkdesigner.uk">Rob Clark Designer</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		<div class="pt3 pb3 text__center section__dark-grey footer__copyright heading__xs heading__grey">
+			<div>Copyright <?php echo date("Y"); ?> ;copy CThrough Windows</div>
+			<div>Website design by Synergy</div>
+		</div>
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
