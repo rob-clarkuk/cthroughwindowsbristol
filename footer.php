568c454dc9944e6dd70e135d2c0e953c2a1ea099
Rob Clark <rob@clarkuk.com>
2022-04-25 12:00:03 +0100

    Footer

--- footer.php
+++ footer.php
@@ -49,11 +49,11 @@
 					<a href="#top" class="ml1 btn btn__secondary heading__xs heading__light">Top of Page</a>
 				</div>
 
-				<div class="pt3 pb3 text__center section__grey footer__navigation">
+				<div class="pt3 pb3 text__center section__grey footer__navigation text__left">
 					<?php if( have_rows('navigation_1') ): ?>
 						<?php while( have_rows('navigation_1') ): the_row();?>
 							<div>
-								<div class="heading__xs heading__light pb1"><?php the_sub_field('title');?></div>
+								<div class="heading__sm heading__primary"><?php the_sub_field('title');?></div>
 							</div>
 						<?php endwhile; ?>
 					<?php endif; ?>
