2f7ee0aa6bf808429c1454916d5cbba8033ca9ff
Rob Clark <rob@clarkuk.com>
2022-04-27 16:54:50 +0100

    CTA Links

--- content/block-ctalinks.php
+++ content/block-ctalinks.php
@@ -1,6 +1,6 @@
 <?php
 	if( have_rows('ctas') ):?>
-	<section class="ctaLinks section__lightgrey pb2 pl3 pr3 pt2 mb2">
+	<section class="ctaLinks section__lightgrey mb2">
 		<div class="grid6 grid ">
 			<?php while( have_rows('ctas') ) : the_row();?>
 				<a class="ctaLinks__item text__center" href="<?php the_sub_field('link');?>">
