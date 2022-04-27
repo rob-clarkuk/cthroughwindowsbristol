2a7dd2a0b904dd2f0d085a07b796efe0ecadd85b
Rob Clark <rob@clarkuk.com>
2022-04-27 09:49:45 +0100

    Home Page Content

--- content/block-cta.php
+++ content/block-cta.php
@@ -1,5 +1,12 @@
-<section class="ctaBlock section__lightgrey pb2 pl3 pr3 pt2 mb2">
-	<div class="grid4 grid">
-		
-	</div>
-</section>
+<?php
+	if( have_rows('ctas') ):?>
+	<section class="ctaBlock section__lightgrey pb2 pl3 pr3 pt2 mb2">
+		<div class="grid4 grid">
+			<?php while( have_rows('ctas') ) : the_row();?>
+				<div class="ctaBlock__item text__center heading__xs">
+					<h4 class="heading__primary"><?php the_sub_field('title');?></h4>
+				</div>
+			<?php endwhile;?>
+		</div>
+	</section>
+<?php endif;?>
