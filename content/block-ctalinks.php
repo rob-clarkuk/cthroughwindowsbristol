cdfe838e03dcd4b20221f99113ad2c939b6524ce
Rob Clark <rob@clarkuk.com>
2022-04-27 16:50:46 +0100

    CTA Links

--- content/block-ctalinks.php
+++ content/block-ctalinks.php
@@ -1,5 +1,12 @@
-<section class="ctaLinks section__lightgrey pb2 pl3 pr3 pt2 mb2">
-	<div class="grid6 grid ">
+<?php
+	if( have_rows('ctas') ):?>
+	<section class="ctaLinks section__lightgrey pb2 pl3 pr3 pt2 mb2">
+		<div class="grid6 grid ">
+			<?php while( have_rows('ctas') ) : the_row();?>
+				<a class="ctaLinks__item text__center heading__xs">
 
-	</div>
-</section>
+				</a>
+			<?php endwhile;?>
+		</div>
+	</section>
+<?php endif;?>
