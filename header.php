423706ef123c1d9c21f7eb77e2fd9b701bc432c0
Rob Clark <rob@clarkuk.com>
2022-04-27 15:24:13 +0100

    Header

--- header.php
+++ header.php
@@ -49,6 +49,17 @@
 					the_custom_logo();
 					?>
 				</div>
+				<nav class="header__nav--navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cthroughwindowsbristol' ); ?></button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
 			</div>
 		</div>
 
@@ -57,15 +68,5 @@
 
 		
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cthroughwindowsbristol' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+		
 	</header><!-- #masthead -->
