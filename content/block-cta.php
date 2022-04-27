1022956feb8969fff87fd639dc6b6269d1acce3d
Rob Clark <rob@clarkuk.com>
2022-04-27 10:13:18 +0100

    Home Page Content

--- content/block-cta.php
+++ content/block-cta.php
@@ -4,6 +4,16 @@
 		<div class="grid4 grid ctaBlock__container">
 			<?php while( have_rows('ctas') ) : the_row();?>
 				<div class="ctaBlock__item text__center heading__xs">
+					<?php 
+					if( get_sub_field('icon') == 'icon-house1' ) {
+					    get_template_part( 'icons/icon', 'house1' )
+					} else if get_sub_field('icon') == 'icon-house2' ){
+						get_template_part( 'icons/icon', 'house2' )
+					} else if get_sub_field('icon') == 'icon-roof' ){
+						get_template_part( 'icons/icon', 'roof' )
+					} else if get_sub_field('icon') == 'icon-buildings' ){
+						get_template_part( 'icons/icon', 'buildings' )
+					};?>
 					<h4 class="heading__primary"><?php the_sub_field('title');?></h4>
 					<div class="pb1 ctaBlock__item--content"><?php the_sub_field('content');?></div>
 					<a href="<?php the_sub_field('link');?>" class="btn btn__primary pl1 pr1"><?php the_sub_field('button_title');?></a>
