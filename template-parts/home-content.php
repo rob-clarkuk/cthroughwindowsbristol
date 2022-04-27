3837b616718db73865db262a7a2002abc37e2bd6
Rob Clark <rob@clarkuk.com>
2022-04-27 09:17:00 +0100

    Home Page Content

--- template-parts/home-content.php
+++ template-parts/home-content.php
@@ -3,7 +3,7 @@
 <main class="pt2 container">
 	<?php while ( have_rows('content') ) : the_row();?>
 
-		<?php if( get_row_layout() == 'text_image_block' ):?>
+		<?php if( get_row_layout() == 'text_&_image_block' ):?>
 			
 			<?php get_template_part( 'content/block', 'textimage' );?>
 
