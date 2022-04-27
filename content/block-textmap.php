4058ab14f429aee1c44ae47896cf625cbcce85a3
Rob Clark <rob@clarkuk.com>
2022-04-27 12:13:21 +0100

    Home Page Content

--- content/block-textmap.php
+++ content/block-textmap.php
@@ -1,1 +1,13 @@
-block-textmap.php
+<section class="textmap grid3 grid mb2">
+	<div class="textmap__content pb2 pl4 pr4 pt2">
+		<?php if(get_sub_field('title')){ ;?>
+			<h3 class="heading__lg heading__body-font font400 heading__light"><?php the_sub_field('title');?></h3>
+		<?php } ;?>
+	</div>
+	<?php if(get_sub_field('image')){ ;?>
+		<?php $image = get_sub_field('image');?>
+		<div class="textmap__image" style="background-image: url(<?php echo esc_url($image['sizes']['textimage-image']); ?>);">
+			
+		</div>
+	<?php } ;?>
+</section>
