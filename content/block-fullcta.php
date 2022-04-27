e306b13a6217e84b9135790e47e681d534e6dd8e
Rob Clark <rob@clarkuk.com>
2022-04-27 11:13:13 +0100

    Home Page Content

--- content/block-fullcta.php
+++ content/block-fullcta.php
@@ -1,1 +1,5 @@
-block-fullcta.php
+<?php $image = get_sub_field('background_image');?>
+<section class="fullcta pt7 pb5" style="background-image: url(<?php echo esc_url($image['sizes']['full']); ?>)">
+
+
+</section>
