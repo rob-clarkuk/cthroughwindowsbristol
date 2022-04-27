6a8d2c217043f6b63e8797efba724f97cd14ca79
Rob Clark <rob@clarkuk.com>
2022-04-27 09:57:32 +0100

    Home Page Content

--- content/block-cta.php
+++ content/block-cta.php
@@ -1,12 +1,12 @@
 <?php
 	if( have_rows('ctas') ):?>
 	<section class="ctaBlock section__lightgrey pb2 pl3 pr3 pt2 mb2">
-		<div class="grid4 grid">
+		<div class="grid4 grid ctaBlock__container">
 			<?php while( have_rows('ctas') ) : the_row();?>
 				<div class="ctaBlock__item text__center heading__xs">
-					<h4 class="heading__secondary"><?php the_sub_field('title');?></h4>
-					<div><?php the_sub_field('content');?></div>
-					<a href="<?php the_sub_field('link');?>" class="btn"><?php the_sub_field('button_title');?></a>
+					<h4 class="heading__primary"><?php the_sub_field('title');?></h4>
+					<div class="pb1"><?php the_sub_field('content');?></div>
+					<a href="<?php the_sub_field('link');?>" class="btn btn__primary pl1 pr1"><?php the_sub_field('button_title');?></a>
 				</div>
 			<?php endwhile;?>
 		</div>
