4948eccefd609a060d37aa51cd54f080c441d564
Rob Clark <rob@clarkuk.com>
2022-04-27 15:19:05 +0100

    Header

--- header.php
+++ header.php
@@ -47,20 +47,7 @@
 				<div class="header__nav--logo">
 					<?php
 					the_custom_logo();
-					if ( is_front_page() && is_home() ) :
-						?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php
-					else :
-						?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-						<?php
-					endif;
-					$cthroughwindowsbristol_description = get_bloginfo( 'description', 'display' );
-					if ( $cthroughwindowsbristol_description || is_customize_preview() ) :
-						?>
-						<p class="site-description"><?php echo $cthroughwindowsbristol_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-					<?php endif; ?>
+					?>
 				</div>
 			</div>
 		</div>
