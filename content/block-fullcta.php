9f372a7b5a292c958ed5443afe95b502e54b0c38
Rob Clark <rob@clarkuk.com>
2022-04-27 11:40:54 +0100

    Home Page Content

--- content/block-fullcta.php
+++ content/block-fullcta.php
@@ -22,10 +22,10 @@
 								<div class="heading__sm heading__body-font text__center font400 heading__light-grey pl1 pr1 pt1 pb1">
 									<?php the_sub_field('content');?>
 								</div>
+								<?php if(get_sub_field('link')){ ;?>
+									<a href="<?php the_sub_field('link');?>" class="pl1 pr1 btn btn__light heading__xs heading__primary">Visit Page</a>
+								<?php } ;?>
 							</div>
-							<?php if(get_sub_field('link')){ ;?>
-								<a href="<?php the_sub_field('link');?>" class="pl1 pr1 btn btn__light heading__xs heading__primary">Visit Page</a>
-							<?php } ;?>
 						<?php } ;?>
 					</div>
 				<?php endwhile;?>
