f5d6e70c16fe0d2973f97029e716be95522f173d
Rob Clark <rob@clarkuk.com>
2022-04-27 12:44:19 +0100

    Home Page Content

--- content/block-textmap.php
+++ content/block-textmap.php
@@ -1,5 +1,5 @@
 <section class="textmap grid3 grid mb2">
-	<div class="textmap__content pb2 pl4 pr4 pt2">
+	<div class="textmap__content pb3 pl4 pr4 pt2">
 		<?php if(get_sub_field('title')){ ;?>
 			<h3 class="heading__md heading__body-font font400 heading__light text__center"><?php the_sub_field('title');?></h3>
 		<?php } ;?>
@@ -13,7 +13,7 @@
 						</div>
 					<?php } ;?>
 					<div class="textmap__content--item__content">
-						<div class="heading__sm font700 heading__light"><?php the_sub_field('title');?></div>
+						<div class="heading__md font700 heading__light"><?php the_sub_field('title');?></div>
 						<div class="heading__body-font font400 heading__light"><?php the_sub_field('content');?></div>
 					</div>
 				</div>
