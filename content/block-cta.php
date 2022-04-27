17ab95735f9d492b0878e08ef0bb2d3e5651b339
Rob Clark <rob@clarkuk.com>
2022-04-27 10:19:16 +0100

    Home Page Content

--- content/block-cta.php
+++ content/block-cta.php
@@ -5,21 +5,10 @@
 			<?php while( have_rows('ctas') ) : the_row();?>
 				<div class="ctaBlock__item text__center heading__xs">
 					<?php 
-
 						if( get_sub_field('icon') == 'icon-house1' ) {
 						     get_template_part( 'icons/icon', 'house1' );
 						}
 					;?>
-					<?php 
-					if( get_sub_field('icon') == 'icon-house1' ) {
-					    get_template_part( 'icons/icon', 'house1' )
-					} elseif ( get_sub_field('icon') == 'icon-house2' ){
-						get_template_part( 'icons/icon', 'house2' )
-					} elseif ( get_sub_field('icon') == 'icon-roof' ){
-						get_template_part( 'icons/icon', 'roof' )
-					} elseif ( get_sub_field('icon') == 'icon-buildings' ){
-						get_template_part( 'icons/icon', 'buildings' )
-					};?>
 					<h4 class="heading__primary"><?php the_sub_field('title');?></h4>
 					<div class="pb1 ctaBlock__item--content"><?php the_sub_field('content');?></div>
 					<a href="<?php the_sub_field('link');?>" class="btn btn__primary pl1 pr1"><?php the_sub_field('button_title');?></a>
