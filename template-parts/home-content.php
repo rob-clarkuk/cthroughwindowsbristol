356d19699bd364f6c47e380ee9d29231ebb68232
Rob Clark <rob@clarkuk.com>
2022-04-27 10:56:30 +0100

    Home Page Content

--- template-parts/home-content.php
+++ template-parts/home-content.php
@@ -1,23 +1,35 @@
 <?php if( have_rows('content') ):?>
 
-<main class="pt2 container">
+<main class="pt2">
 	<?php while ( have_rows('content') ) : the_row();?>
 
 		<?php if( get_row_layout() == 'text_&_image_block' ):?>
+
+		<div class="container">
 			
 			<?php get_template_part( 'content/block', 'textimage' );?>
+
+		</div>
 
 		<?php endif ;?>
 
 		<?php if( get_row_layout() == 'text_image_combine_block' ):?>
+
+		<div class="container">
 			
 			<?php get_template_part( 'content/block', 'combinedtextimage' );?>
+
+		</div>
 
 		<?php endif ;?>
 
 		<?php if( get_row_layout() == 'cta_block' ):?>
+
+		<div class="container">
 			
 			<?php get_template_part( 'content/block', 'cta' );?>
+
+		</div>
 
 		<?php endif ;?>
 
