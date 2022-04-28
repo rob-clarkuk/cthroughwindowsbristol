277fc83a5c577dd883fafd55e8d4725740869402
Rob Clark <rob@clarkuk.com>
2022-04-28 09:59:41 +0100

    Responsivness

--- js/navigation.js
+++ js/navigation.js
@@ -97,3 +97,17 @@
 		}
 	}
 }() );
+
+/* global wp, jQuery */
+/**
+ * File customizer.js.
+ *
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+
+( function( $ ) {
+	
+	console.log('working');
+}( jQuery ) );
